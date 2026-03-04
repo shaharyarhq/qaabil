ce674ff8efb96fd31682a61850923f12ca94b99e
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-03-04 15:22:56 +0500

    Modified Videos Table

--- app/Filament/Student/Resources/Videos/Tables/VideosTable.php
+++ app/Filament/Student/Resources/Videos/Tables/VideosTable.php
@@ -2,12 +2,17 @@
 
 namespace App\Filament\Student\Resources\Videos\Tables;
 
+use App\Filament\Support\Components\Filters\CreatedAtFilter;
+use App\Filament\Support\Components\Filters\UpdatedAtFilter;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class VideosTable
 {
@@ -15,6 +20,17 @@ class VideosTable
     {
         return $table
             ->columns([
+                ImageColumn::make('thumbnail_url')
+                    ->label('Video Thumbnail')
+                    ->circular()
+                    ->imageSize(80)
+                    ->state(function (?Model $record) {
+                        $guid = $record->video_url;
+                        $cdnHostName = config('filesystems.disks.bunny_stream.hostname');
+                        $url = "https://{$cdnHostName}/{$guid}/{$record->thumbnail_url}";
+                        return $url;
+                    })
+                    ->searchable(),
                 TextColumn::make('course.name')
                     ->copyable(),
                 TextColumn::make('chapter.name')
@@ -23,10 +39,6 @@ class VideosTable
                     ->copyable(),
                 TextColumn::make('title')
                     ->copyable(),
-                TextColumn::make('video_url')
-                    ->searchable(),
-                TextColumn::make('thumbnail_url')
-                    ->searchable(),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -37,7 +49,14 @@ class VideosTable
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                CreatedAtFilter::make(),
+                UpdatedAtFilter::make(),
+                SelectFilter::make('course')
+                    ->relationship('course', 'name'),
+                SelectFilter::make('chapter')
+                    ->relationship('chapter', 'name'),
+                SelectFilter::make('objective')
+                    ->relationship('objective', 'name'),
             ])
             ->recordActions([
                 // ViewAction::make(),

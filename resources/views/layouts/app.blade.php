c53038cf37aa384ed3f304f6467fb12effdacdcc
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-05-07 12:45:51 +0500

    Added slider to hero section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
     <meta property="og:description" content="Empowering Learning" />
     {{-- <meta property="og:image" content="{{ asset('images/logo/favicon.png') }}" /> --}}
-    <meta property="og:image" content="https://cdn.savemyexams.com/images/misc/save-my-exams-og.jpg" />
+    <meta property="og:image" content="{{ asset('images/logo/og-banner-v2.png') }}" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta property="og:image:type" content="image/png" />

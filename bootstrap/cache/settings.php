1e0ed61b9ae67897d72adcb990086da4398c8973
Shaharyar Ahmed <shery.codes@gmail.com>
2026-07-23 12:31:29 +0500

    Commit

--- bootstrap/cache/settings.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  0 => 'App\\Settings\\PricingPageSettings',
-  1 => 'App\\Settings\\ContactPageSettings',
-  2 => 'App\\Settings\\PrivacyPolicyPageSettings',
-  3 => 'App\\Settings\\ApplicationSettings',
-  4 => 'App\\Settings\\AcademyPageSettings',
-  5 => 'App\\Settings\\TermsAndConditionsPageSettings',
-  6 => 'App\\Settings\\HomePageSettings',
-);
